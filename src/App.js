f589d75ca6bbe962dd6224e38ff3097381791d00
Ben Jason <benjason777@gmail.com>
2019-07-16 17:55:00 -0700

    using css grid instead of bootstrap for app.js styling

--- src/App.js
+++ src/App.js
@@ -40,12 +40,11 @@ class App extends Component {
     const currentUser =
       this.state.user === null ? "Guest" : this.state.user.displayName;
     return (
-      <div className="container myContainer">
-        <div className="row myRow fixed-top">
-          <div className="col-3 myCol ">
-            <img className="img" alt="logo" src={brand} />
-          </div>
-          <div className="col-9  myBar">
+      <div className="myContainer">
+        <div className="myTopNav">
+          <img className="img" alt="logo" src={brand} />
+
+          <div className="loginAndOut">
             <User
               firebase={firebase}
               setUser={this.setUser.bind(this)}
@@ -55,26 +54,21 @@ class App extends Component {
           </div>
         </div>
         <div className="sperationLine fixed-top" />
-        <div className="row">
-          <div className="col-sm-12 col-md-4 rooms">
-            <div>
-              <RoomList
-                firebase={firebase}
-                selectRoom={this.selectRoom.bind(this)}
-                activeRoom={this.state.activeRoom}
-              />
-            </div>
+        <div className="RoomsAndTextArea">
+          <div style={{ backgroundColor: "#151a57" }}>
+            <RoomList
+              firebase={firebase}
+              selectRoom={this.selectRoom.bind(this)}
+              activeRoom={this.state.activeRoom}
+            />
           </div>
 
-          <div className="col-sm-12 col-md-8">
-            <div className={showMessages}>
-              <div />
-              <MessageList
-                firebase={firebase}
-                activeRoom={this.state.activeRoom}
-                currentUser={currentUser || "guest"}
-              />
-            </div>
+          <div className={showMessages}>
+            <MessageList
+              firebase={firebase}
+              activeRoom={this.state.activeRoom}
+              currentUser={currentUser || "guest"}
+            />
           </div>
         </div>
       </div>
